1cacd8d73867624eb6431aa285b30d9f567d548a
123456 <wangggg28@outlook.com>
2023-03-03 09:39:15 +0800

    第二次

--- src/test/java/com/xuecheng/XuechengPlusContentServiceApplicationTests.java
+++ src/test/java/com/xuecheng/XuechengPlusContentServiceApplicationTests.java
@@ -1,13 +1,22 @@
 package com.xuecheng;
 
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.po.CourseBase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XuechengPlusContentServiceApplicationTests {
 
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
+
     @Test
     void contextLoads() {
+        CourseBase courseBase = courseBaseMapper.selectById(74L);
+        Assertions.assertNotNull(courseBase);
     }
 
 }
